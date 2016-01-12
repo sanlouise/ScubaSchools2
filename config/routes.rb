46b88698f59ddc13d214bcce6f038f5cbf464550
sandralouise <sandra.adamshallie@gmail.com>
2016-01-12 13:33:45 +0000

    Add bootstrap and edit css forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :schools
-  get 'pages/about'
+  get '/about', to: 'pages#about'
 
-  get 'pages/contact'
+  get '/contact', to:'pages#contact'
   
   root 'schools#index'
 
