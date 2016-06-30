fb9c2a6f3d54f989fbb9adc5569bfd9273798264
sandralouise <sandra.adamshallie@gmail.com>
2016-06-30 06:42:34 +0000

    Fix AWS deployment issue

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',                        # required
     aws_access_key_id:      ENV['aws_access_key_id'],                        # required
-    aws_secret_access_key:  ENV['aws_secret_access_key_id'],  
+    aws_secret_access_key:  ENV['aws_secret_access_key'],  
     region:                 'eu-central-1'
 
   }
